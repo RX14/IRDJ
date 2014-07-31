6bfd06caeac3819708f9a0c3ee04f669c625cfb2
RX14 <chris@rx14.co.uk>
2014-07-31 21:09:26 +0100

    Update conf.example.rb

--- conf.example.rb
+++ conf.example.rb
@@ -3,7 +3,11 @@
     conf.channels = ["#mytwitchchannel"]
     conf.nick     = "twitchbot1000"
     conf.password = "oauth:blablabla"
+    
+    #Plugin class names, generally the names of the files.
     conf.plugins.plugins = [IRDJ, JoinMessage]
 end
 
+#$pconf is a key-value store used by plugins for configuration.
+#Here we set "JOIN_MESSAGES" to a string aray for the JoinMessage plugin.
 $pconf["JOIN_MESSAGES"] = ["/color GoldenRod"]
