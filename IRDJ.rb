2221912366d992a043dae120b7df31a2b7d7d284
RX14 <chris@rx14.co.uk>
2014-07-31 21:05:41 +0100

    Make the bot global and comment on the global plugin config hash
    @Lordmau5

--- IRDJ.rb
+++ IRDJ.rb
@@ -9,16 +9,17 @@ class Cinch::Message
     end
 end
 
+#global plugin config hash
 $pconf = {}
 
 #Load plugin files
 Dir[File.dirname(__FILE__) + '/plugins/*.rb'].each {|file| require file }
 
-@bot = Cinch::Bot.new do
+$bot = Cinch::Bot.new do
     on :message, "!IRDJ" do |m|
         m.reply "I am IRDJ, a SUPERIOR set of cinchrb plugins made for Lordmau5"
     end
 end
 require_relative "conf"
 
-@bot.start
+$bot.start
