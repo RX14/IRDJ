70a84d48bef019ac708a4589e85f028f3fd37867
RX14 <chris@rx14.co.uk>
2014-08-27 18:46:35 +0100

    Added hacky Queue class with ability to see queued items

---
+++ lib/SimpleQueue.rb
@@ -0,0 +1,59 @@
+require 'thread'
+
+class SimpleQueue
+    include Enumerable
+
+    attr_reader :store
+    attr_reader :mutex
+
+    def initialize
+        @store   = Array.new
+        @mutex   = Mutex.new
+    end
+
+    def pop
+        r = nil
+        loop do
+            @mutex.synchronize { r = @store.shift }
+            return r unless r == nil
+            sleep 0.1
+        end
+    end
+
+    def push(object)
+        @mutex.synchronize do
+            @store.push(object)
+        end
+        return object
+    end
+
+    def size
+        @store.size
+    end
+
+    def to_a
+        @store
+    end
+
+    def [](index)
+        @store[index]
+    end
+
+    def []=(index, object)
+        @mutex.synchronize { @store[index] = object }
+    end
+
+    def <<(object)
+        push(object)
+    end
+
+    def each(&block)
+        @store.each do |val|
+            if block_given?
+                block.call val
+            else
+                yield val
+            end
+        end
+    end
+end
